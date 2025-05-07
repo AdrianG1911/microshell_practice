d8df0cb1d2577dc07f919ea5f1f99d0b5381c13f
Adrian Gutierrez Valero <adrgutie@c5r3s8.42tokyo.jp>
2025-05-07 16:38:28 +0900

    done

--- microshell.c
+++ microshell.c
@@ -25,15 +25,46 @@ int	cd_cmd(char *argv[], int i)
 	return (0);
 }
 
+int	redirect_pipe(int in_out[], int to_pipe, int in_or_out)
+{
+	if (to_pipe == 0)
+		return (0);
+	if (dup2(in_out[in_or_out], in_or_out) == -1 || \
+	close(in_out[0]) == -1 || close(in_out[1]) == -1)
+		return (puterr("error: fatal\n"), exit (1), 1);
+	return (0);
+}
+
 int	gen_exec(char *argv[], int i, char *envp[])
 {
-	int		pipe[2];
+	int		in_out[2];
 	pid_t	pid;
 	int		to_pipe;
+	int		status;
 
 	to_pipe = 0;
-	if (strcmp(argv[i], "|") == 0)
+	if (argv[i] != NULL && strcmp(argv[i], "|") == 0)
 		to_pipe = 1;
+	if (to_pipe != 1 && strcmp("cd", argv[0]) == 0)
+		return (cd_cmd(argv, i));
+	if (to_pipe == 1 && pipe(in_out) == -1)
+		return (puterr("error: fatal\n"), exit (1), 1);
+	pid = fork();
+	if (pid == -1)
+		return (puterr("error: fatal\n"), exit (1), 1);
+	if (pid == 0)
+	{
+		argv[i] = NULL;
+		redirect_pipe(in_out, to_pipe, STDOUT_FILENO);
+		if (strcmp(argv[0], "cd") == 0)
+			exit(cd_cmd(argv, i));
+		execve(argv[0], argv, envp);
+		return (puterr("error: cannot execute "), \
+		puterr(argv[0]), puterr("\n"), exit(1), 1);
+	}
+	waitpid(pid, &status, 0);
+	redirect_pipe(in_out, to_pipe, STDIN_FILENO);
+	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
 
 int	main(int argc, char *argv[], char *envp[])
@@ -43,14 +74,16 @@ int	main(int argc, char *argv[], char *envp[])
 
 	if (argc == 1)
 		return (0);
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (argv[i] != NULL)
 	{
-		argv += i;
+		argv += i + 1;
 		i = 0;
 		while (argv[i] != NULL && strcmp(argv[i], "|") != 0 && \
 								strcmp(argv[i], ";") != 0)
 			i++;
-		status = gen_exec(argv, i, envp);
+		if (i)
+			status = gen_exec(argv, i, envp);
 	}
+	return (status);
 }
